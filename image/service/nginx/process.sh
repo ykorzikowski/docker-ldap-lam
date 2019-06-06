31e7a18434cda3e531df04a35aacc2e36488fcff
root <root@localhost.localdomain>
2019-06-06 17:08:18 +0200

    removing ldap server

--- image/service/nginx/process.sh
+++ image/service/nginx/process.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
+log-helper level eq trace && set -x
+
 echo "Starting nginx..."
 exec /usr/sbin/nginx -g "daemon off;"
--- image/service/nginx/startup.sh
+++ image/service/nginx/startup.sh
@@ -1,1 +1,10 @@
 #!/bin/bash -e
+
+FIRST_START_DONE="${CONTAINER_STATE_DIR}/nginx-first-start-done"
+
+# container first start
+if [ ! -e "$FIRST_START_DONE" ]; then
+  touch $FIRST_START_DONE
+fi
+
+exit 0
