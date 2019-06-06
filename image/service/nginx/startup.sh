a101d9f9e9feac1b663ad11550921c143c1b2770
root <root@localhost.localdomain>
2019-06-06 17:57:44 +0200

    wip

--- image/service/nginx/startup.sh
+++ image/service/nginx/startup.sh
@@ -4,6 +4,8 @@ FIRST_START_DONE="${CONTAINER_STATE_DIR}/nginx-first-start-done"
 
 # container first start
 if [ ! -e "$FIRST_START_DONE" ]; then
+  cp -nr /usr/share/ldap-account-manager/config.example/* /usr/share/ldap-account-manager/config/
+  chown www-data /usr/share/ldap-account-manager/config/
   touch $FIRST_START_DONE
 fi
 
